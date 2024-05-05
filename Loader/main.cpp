59b4a5ebd46e5e2c09006fce242121e1a40fe17e
PotatoPrn <PotatoPrn@proton.me>
2024-05-05 20:28:01 +1000

    Added so the Loader can be Dynamically Tested

--- Loader/main.cpp
+++ Loader/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <SubAuth.h>
+#include <lmcons.h>
 
 using _FilterPassword = BOOLEAN(*)(PUNICODE_STRING AccountName, PUNICODE_STRING FullName, PUNICODE_STRING Password, BOOLEAN IsSet);
 
@@ -22,19 +23,36 @@ PUNICODE_STRING ConvertToPuni(char* Character)
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
 	// Passfilt.dll
 	// Build Project in 32bit if using 32bit Passfilt.dll & ViseVersa
+	if (argc != 2)
+	{
+		printf("Input Password");
+		exit(1);
+	}
+
+	// Get Username
+	char Username[UNLEN+1];
+	DWORD UsernameLen[UNLEN+1];
+	GetUserName(Username, UsernameLen);
+
+	// Get Account/Computer Name
+	char ComputerName[UNLEN+1];
+	DWORD ComputerNameLen[UNLEN+1];
+	GetComputerName(ComputerName, ComputerNameLen);
 
 	_FilterPassword FilterPassword =
 			reinterpret_cast<_FilterPassword>(GetProcAddress(LoadLibrary("PassfiltLib.dll"), "PasswordFilter"));
 
 
 	PUNICODE_STRING AccountName, Fullname, Password;
-	AccountName = ConvertToPuni("Fuck");
-	Fullname = ConvertToPuni("You");
-	Password = ConvertToPuni("Cunt");
+	AccountName = ConvertToPuni(ComputerName);
+	Fullname = ConvertToPuni(Username);
+	Password = ConvertToPuni(argv[1]);
+
+	printf("Input Results Username > %s, Accountname > %s, Password > %s\n", Username, ComputerName, argv[1]);
 
 	bool Result = FilterPassword(AccountName, Fullname, Password, 0);
 
---
+++ README.md
@@ -0,0 +1,2 @@
+## WARNING
+POC STUFF, Learning how Password filter works and integrating it with zxcvb-c
