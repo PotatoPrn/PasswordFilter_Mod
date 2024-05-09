5be2c0d67acb77d02a1d83e6494cd99559c3e8fa
PotatoPrn <PotatoPrn@proton.me>
2024-05-09 20:18:44 +1000

    Removed the Print Result Check and had it return based on score, added a secure wipe for the password

--- PassFiltLib/Utils.cpp
+++ PassFiltLib/Utils.cpp
@@ -9,5 +9,6 @@ char* PuniToChar(PUNICODE_STRING PString)
 	size_t NewSize = OrigSize * 2;
 	char* PasswordString = new char[NewSize];
 	wcstombs_s(&ConvertedChar, PasswordString, NewSize, PString->Buffer, _TRUNCATE);
+	SecureZeroMemory(PString->Buffer, OrigSize);
 	return PasswordString;
 }
--- PassFiltLib/main.cpp
+++ PassFiltLib/main.cpp
@@ -8,106 +8,21 @@
 #include "Utils.h"
 #include "zxcppvbn.hpp"
 
-#define LIBRARY_API __declspec(dllexport)
-
 extern "C"
 {
-LIBRARY_API BOOLEAN PasswordFilter(PUNICODE_STRING PuniAccountName, PUNICODE_STRING PuniFullname, PUNICODE_STRING PuniPassword, BOOLEAN SetOperation);
+__declspec(dllexport) BOOLEAN _fastcall PasswordFilter(PUNICODE_STRING PuniAccountName, PUNICODE_STRING PuniFullname, PUNICODE_STRING PuniPassword, BOOLEAN SetOperation);
 }
 
 
-void render_match(const zxcppvbn::match& match)
+BOOLEAN _fastcall PasswordFilter(PUNICODE_STRING PuniAccountName, PUNICODE_STRING PuniFullname, PUNICODE_STRING PuniPassword, BOOLEAN SetOperation)
 {
-	std::cout << "  token: " << match.token << std::endl;;
-	std::cout << "   pattern: ";
-	switch (match.pattern)
-	{
-	case zxcppvbn::pattern::L33T:
-		std::cout << "L33T";
-		break;
-	case zxcppvbn::pattern::DICTIONARY:
-		std::cout << "DICTIONARY";
-		break;
-	case zxcppvbn::pattern::SPATIAL:
-		std::cout << "SPATIAL";
-		break;
-	case zxcppvbn::pattern::REPEAT:
-		std::cout << "REPEAT";
-		break;
-	case zxcppvbn::pattern::SEQUENCE:
-		std::cout << "SEQUENCE";
-		break;
-	case zxcppvbn::pattern::DATE:
-		std::cout << "DATE";
-		break;
-	case zxcppvbn::pattern::BRUTEFORCE:
-		std::cout << "BRUTEFORCE";
-		break;
-	}
-	std::cout << ", i: " << match.i << ", j: " << match.j;
-	std::cout << ", entropy: " << match.entropy << std::endl;
-	switch (match.pattern)
-	{
-	case zxcppvbn::pattern::L33T:
-		std::cout << "   subs: " << match.sub_display;
-		std::cout << ", l33t entropy: " << match.l33t_entropy << std::endl;
-	case zxcppvbn::pattern::DICTIONARY:
-		std::cout << "   dictionary: " << match.dictionary_name;
-		std::cout << ", word: " << match.matched_word;
-		std::cout << ", rank: " << match.rank;
-		std::cout << ", base entropy: " << match.base_entropy;
-		std::cout << ", uppercase entropy: " << match.uppercase_entropy << std::endl;
-		break;
-	case zxcppvbn::pattern::SPATIAL:
-		std::cout << "   keyboard: " << match.graph;
-		std::cout << ", turns: " << match.turns;
-		std::cout << ", shift count: " << match.shifted_count << std::endl;
-		break;
-	case zxcppvbn::pattern::REPEAT:
-		std::cout << "   repeated char: " << match.repeated_char << std::endl;
-		break;
-	case zxcppvbn::pattern::SEQUENCE:
-		std::cout << "   sequence name: " << match.sequence_name;
-		std::cout << ", sequence space: " << match.sequence_space;
-		std::cout << ", ascending: " << match.ascending << std::endl;
-		break;
-	case zxcppvbn::pattern::DATE:
-		std::cout << "   year: " << match.year;
-		std::cout << ", month: " << match.month;
-		std::cout << ", day: " << match.day;
-		std::cout << ", separator: " << match.separator << std::endl;
-		break;
-	case zxcppvbn::pattern::BRUTEFORCE:
-		std::cout << "   cardinality: " << match.cardinality << std::endl;
-		break;
-	}
-}
+	BOOLEAN Status = TRUE;
 
-void render_result(const zxcppvbn::result& result)
-{
-	std::cout << "password: " << result.password << std::endl;
-	std::cout << " entropy: " << result.entropy;
-	std::cout << ", crack time: " << result.crack_time_display << " (" << result.crack_time.count() << " s)";
-	std::cout << ", score: " << result.score;
-	std::cout << ", calculation time: " << result.calc_time.count() << " ms" << std::endl;
-	std::cout << " matches: " << std::endl;
-	for (auto& match: result.matches)
-	{
-		render_match(*match);
-	}
-}
-
-BOOLEAN PasswordFilter(PUNICODE_STRING PuniAccountName, PUNICODE_STRING PuniFullname, PUNICODE_STRING PuniPassword, BOOLEAN SetOperation)
-{
-	BOOLEAN Status = FALSE;
-
-
-	// Just for you, you know who .-.
+	// Just for you f3rnos
 	if (SetOperation != 0)
+	{
 		return Status;
-
-
-	// If Using Dict :/
+	}
 
 
 	// Convert to Narrow String
@@ -115,16 +30,26 @@ BOOLEAN PasswordFilter(PUNICODE_STRING PuniAccountName, PUNICODE_STRING PuniFull
 	char* Fullname = PuniToChar(PuniFullname);
 	char* AccountName = PuniToChar(PuniAccountName);
 
-	// Test User Dict
-	zxcppvbn Zxcvbn;
-	zxcppvbn::result result = Zxcvbn(Password);
-	render_result(result);
 
-
+	// This only contains a basic check via password entropy, i want to implement the user & password strcompare etc etc
 	// Perform Check
+	zxcppvbn Zxcvbn;
+	zxcppvbn::result Result = Zxcvbn(Password);
 
-	// Cleanup
+	printf("Strength > %d\n", Result.score);
 
+	if (Result.score < 4)
+	{
+		Status = FALSE;
+	}
+
+	// Username & Account name to password comparison
+	char* CompCheck = strstr(_strupr(Password), _strupr(Fullname));
+	if (CompCheck != 0)
+	{
+		printf("Password Contains Username\n");
+		Status = FALSE;;
+	}
 
 	delete[] Password;
 	delete[] Fullname;
